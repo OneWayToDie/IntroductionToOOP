d1abcdd6b8b3ea337b896fb02c43763d41c8847c
Danila <Danila@DESKTOP-44LP0PV>
2025-07-19 22:47:38 +0300

    I'm Mr. DNOOOOOOOO

--- IntroductionToOOP/main.cpp
+++ IntroductionToOOP/main.cpp
@@ -1,37 +1,5 @@
 ﻿#include"Point.h"
 
-
-double distance(const Point& A,const Point& B)
-{
-	/*A.set_x(A.get_x() *= 100);*/
-	double x_distance = A.get_x() - B.get_x();
-	double y_distance = A.get_y() - B.get_y();
-	double distance = sqrt(x_distance * x_distance + y_distance * y_distance);
-	return distance;
-}
-
-Point operator+(const Point& left, const Point& right)
-{
-	Point result;
-	result.set_x(left.get_x() + right.get_x());
-	result.set_y(left.get_y() + right.get_y());
-	return result;
-}
-
-bool operator==(const Point& left, const Point& right)
-{
-	/*if (left.get_x() == right.get_x() && left.get_y() == right.get_y())
-		return true;
-	else
-		return false;*/
-	return left.get_x() == right.get_x() && left.get_y() == right.get_y();
-}
-
-bool operator!=(const Point& left, const Point& right)
-{
-	return !(left == right);
-}
-
 //#define STRUCT_POINT
 //#define distance_check
 //#define constructors_check
